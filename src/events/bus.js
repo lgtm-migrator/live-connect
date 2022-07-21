3d2425825d1d4dc0f3043f01cca5ebc00b9724e9
wiem <welabidine@liveintent.com>
2022-07-21 14:18:49 +0200

    implement busWithBestLIConfiguration

--- src/events/bus.js
+++ src/events/bus.js
@@ -27,15 +27,40 @@ export function init (size, errorCallback) {
 
 export function registerBus (privateBus) {
   try {
-    console.log('events.bus.init')
+    console.log('events.bus.registerBus')
     if (!window) {
       privateBus.error(new Error('Bus can only be attached to the window, which is not present'))
     } else {
-      window[C.EVENT_BUS_NAMESPACE] = privateBus
+      window[C.EVENT_BUS_NAMESPACE] = busWithBestLIConfiguration(privateBus)
     }
     return window[C.EVENT_BUS_NAMESPACE]
   } catch (e) {
     console.error('events.bus.init', e)
-    privateBus.error(new Error('Failied to attach message bus to window'))
+    privateBus.error(new Error('Failed to attach message bus to window'))
   }
+}
+
+export function busWithBestLIConfiguration (privateBus) {
+  const liConfigs = []
+  if (privateBus && privateBus.q && privateBus.q.li_config) {
+    console.log('liConfig >> ' + JSON.stringify(privateBus.q.li_config))
+    liConfigs.push(privateBus.q.li_config)
+  }
+
+  if (window && window[C.EVENT_BUS_NAMESPACE] && window[C.EVENT_BUS_NAMESPACE].q && window[C.EVENT_BUS_NAMESPACE].q.li_config) {
+    liConfigs.push(window[C.EVENT_BUS_NAMESPACE].q.li_config.flat())
+  }
+
+  const flattenLiConfigs = liConfigs.flat(Infinity)
+
+  if (flattenLiConfigs && flattenLiConfigs.length > 0) {
+    for (var i = 0; i < flattenLiConfigs.length; i++) {
+      if (flattenLiConfigs[i] && flattenLiConfigs[i].appId) {
+        privateBus.q.li_config = [flattenLiConfigs[i]]
+        return privateBus
+      }
+    }
+    privateBus.q.li_config = [flattenLiConfigs[0]]
+    return privateBus
+  } else return privateBus
 }
--- test/unit/events/bus.spec.js
+++ test/unit/events/bus.spec.js
@@ -1,5 +1,6 @@
 import { expect, use } from 'chai'
 import * as bus from '../../../src/events/bus'
+import E from '../../../src/events/replayemitter'
 import * as C from '../../../src/utils/consts'
 import jsdom from 'mocha-jsdom'
 import dirtyChai from 'dirty-chai'
@@ -34,6 +35,41 @@ describe('EventsBus in a window', () => {
   it('should set the size correctly', function () {
     const eventBus = bus.init(3)
     expect(eventBus.size).to.eq(3)
+  })
+})
+
+describe('registerBus', () => {
+  jsdom({
+    url: 'http://www.example.com',
+    useEach: true
+  })
+
+  it('should register the private bus with the defined single configuration', function () {
+    const privateBus = new E(5)
+    const config = { trackerName: 'v1.0.1' }
+    const firstBus = window[C.EVENT_BUS_NAMESPACE]
+    privateBus.emit('li_config', config)
+    bus.registerBus(privateBus)
+    const secondBus = window[C.EVENT_BUS_NAMESPACE]
+    expect(firstBus).to.not.eql(secondBus)
+    expect(secondBus.q.li_config).to.eql([config])
+  })
+
+  it('should register the private bus with the first configuration that has a defined appId', function () {
+    const privateBus = new E(5)
+    bus.init()
+    const config1 = { trackerName: 'v1.0.1' }
+    const config2 = { trackerName: 'v1.0.2' }
+    const config3 = { trackerName: 'v1.0.3', appId: 'app-0147' }
+    const config4 = { trackerName: 'v1.0.4', appId: 'app-0148' }
+    const config5 = { trackerName: 'v1.0.5' }
+    const firstBus = window[C.EVENT_BUS_NAMESPACE]
+    firstBus.emit('li_config', config1)
+    privateBus.emit('li_config', [config2, config3, config4, config5])
+    bus.registerBus(privateBus)
+    const secondBus = window[C.EVENT_BUS_NAMESPACE]
+    expect(firstBus).to.not.eql(secondBus)
+    expect(secondBus.q.li_config).to.eql([config3])
   })
 })
 
